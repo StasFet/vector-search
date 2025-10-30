3d06f9c73195198c02f066a5ea7baa73a817934a
pl1z <51047441+StasFet@users.noreply.github.com>
2025-10-30 20:50:12 +1000

    fix: connection works

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 func main() {
-	fmt.Println("Hello World!")
-	
-	if uri := os.Getenv("MONGODB_URI"); uri == "" {
+	// fmt.Println("Hello World!")
+
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
 		log.Fatalf("You must set your 'MONGODB_URI' environment variable")
 	}
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	var result bson.M
+	err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("The deployment has been successfully pinged! The connection is made.")
 }
