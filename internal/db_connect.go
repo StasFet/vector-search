be2ea114479aedb6d7375396499bd73f8e3ad069
pl1z <51047441+StasFet@users.noreply.github.com>
2025-10-31 19:14:05 +1000

    refactor: put db logic into db_connect.go and added functions for connection and ping

---
+++ internal/db_connect.go
@@ -0,0 +1,40 @@
+package internal
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func ConnectToMongo() (*mongo.Client, error) {
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatalf("You must set your 'MONGODB_URI' environment variable")
+	}
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	return client, nil
+}
+
+func PingDB(client *mongo.Client) (success bool) {
+	var result bson.M
+	err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result)
+	return err == nil
+}
--- main.go
+++ main.go
@@ -1,52 +1,19 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"os"
 	i "mongo_vector_search/internal"
-
-	"go.mongodb.org/mongo-driver/v2/bson"
-	"go.mongodb.org/mongo-driver/v2/mongo"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 func main() {
 	// fmt.Println("Hello World!")
-
-	var uri string
-	if uri = os.Getenv("MONGODB_URI"); uri == "" {
-		log.Fatalf("You must set your 'MONGODB_URI' environment variable")
-	}
-
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
-
-	client, err := mongo.Connect(opts)
+	client, err := i.ConnectToMongo()
 	if err != nil {
-		panic(err)
+		log.Fatalf("error connecting to mongo: %v", err)
 	}
 
-	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
-
-	var result bson.M
-	err = client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result)
-	if err != nil {
-		panic(err)
+	if ping := i.PingDB(client); !ping {
+		log.Fatalf("error: cannot ping db")
 	}
-	fmt.Println("The deployment has been successfully pinged! The connection is made.")
-
-	// temporary test
-	testString := "Hello World!"
-	embedding, err := i.GetVectorEmbedding(testString)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Len of embedding of '%s' : %v\n", testString, len(*embedding.GetVector()))
 
 }
